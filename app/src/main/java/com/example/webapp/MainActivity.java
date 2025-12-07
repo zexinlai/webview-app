61cb53b3db196313a6f8b5cd52bd25229f081502
zexinlai <36807459+zexinlai@users.noreply.github.com>
2025-12-07 16:19:59 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/example/webapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.webapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish(); // 按返回键退出应用
+    }
+}
